5e98be62958bd686202a354d8cb7ac251ae73a45
mikewiegering <mike.wiegering@gmail.com>
2020-06-12 11:28:41 -0700

    added def init

--- divisors_app.py
+++ divisors_app.py
@@ -2,8 +2,11 @@
 
 class Divisors:
     
+    def _init_(self):
+        self.user_num = 0
+        self.divisors = []
+
     def set_user_num(self):
         self.user_num = int(input("Enter a natural number: "))
 
     def generate_divisors(self):
-    pass
